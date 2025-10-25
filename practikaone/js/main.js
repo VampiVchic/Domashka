9569ff9aac38fafa345eef87ce47b01678efcf41
VampiVchic <85443161+VampiVchic@users.noreply.github.com>
2025-10-25 07:22:18 +0300

    Add files via upload

--- practikaone/js/main.js
+++ practikaone/js/main.js
@@ -1,22 +1,38 @@
-(function() {
-    const body = document.body;
-    const burger = document.querySelector('.burger-icon');
-    const nav = document.querySelector('.nav');
+(function () {
+    document.addEventListener('click', burgerInit)
 
-    burger.addEventListener('click', e => {
-        e.preventDefault();
-        body.classList.toggle('body--opened-menu');
-    });
+    function burgerInit(e) {
+        const burgerIcon = e.target.closest('.burger-icon')
+        const burgerNavLink = e.target.closest('.nav__link')
 
-    const navLinks = document.querySelectorAll('.nav__link');
-    
-    if (document.documentElement.clientWidth > 900) return
+        if (!burgerIcon && !burgerNavLink) return
+        if (document.documentElement.clientWidth > 900) return
 
-    navLinks.forEach(link => {
-        link.addEventListener('click', e => {
-            if (body.classList.contains('body--opened-menu')) {
-                body.classList.remove('body--opened-menu');
-            }
-        });
-    });
+        if (!document.body.classList.contains('body--opened-menu')) {
+            document.body.classList.add('body--opened-menu')
+        } else {
+            document.body.classList.remove('body--opened-menu')
+        }
+    }
+
+    const modal = document.querySelector('.modal')
+    const modalButton = document.querySelector('.about__img-button')
+
+    modalButton.addEventListener('click', openModal)
+    modal.addEventListener('click', closeModal)
+
+    function openModal(e) {
+        e.preventDefault()
+        document.body.classList.toggle('body--opened-modal')
+    }
+
+    function closeModal(e) {
+        e.preventDefault()
+
+        const target = e.target
+
+        if (target.closest('.modal__cancel') || target.classList.contains('modal')) {
+            document.body.classList.remove('body--opened-modal')
+        }
+    }
 })();
